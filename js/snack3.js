d887706ac99c128a3bf6d3e23576e3ef9dce2b43
RobertoZeppilli <robzep171@gmail.com>
2021-05-11 15:42:18 +0200

    Snack3 (commento)

--- js/snack3.js
+++ js/snack3.js
@@ -9,8 +9,8 @@ function intervalArray(arr, a, b) {
     return newArray;
 };
 
-// 2. La funzione restituirà un nuovo array con i valori che hanno la posizione compresa tra i due numeri (inclusi).
+// 2. La funzione restituirà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 console.log("Array originale", array);
-var nuovoArray = intervalArray(array, 5, 10);
+var nuovoArray = intervalArray(array, 1, 5);
 console.log("Array tagliato", nuovoArray);
