66e358dae2985fddb919effdfc99e951fa7dbd5e
RobertoZeppilli <robzep171@gmail.com>
2021-05-11 16:48:53 +0200

    Snack1 (con funzione)

--- js/snack1.js
+++ js/snack1.js
@@ -1,34 +1,36 @@
 // Snack 1
-// Creare un array di oggetti:
+// 1. funzione per stabilire quale sia la chiave con il minor valore all'interno di un array di oggetti
+function peso(arr, key) {
+    var biciLeggera = arr[0];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i][key] < biciLeggera[key]) {
+            biciLeggera = biciDaCorsa[i];
+        }
+    }
+    return biciLeggera;
+}
+// 2. Creare un array di oggetti:
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 var biciDaCorsa = [
     {
         nome: "Atala",
-        peso: 30
+        peso: 40
     },
     {
         nome: "Bianchi",
-        peso: 12
+        peso: 10
     },
     {
         nome: "Trek",
-        peso: 2
+        peso: 30
     },
     {
         nome: "Paco",
-        peso: 18
+        peso: 15
     },
 ]
 console.log(biciDaCorsa);
 
-// inizializzare una variabile biciLeggera per poi comparare nel ciclo for quale peso sia il minore tra tutti
-var biciLeggera = biciDaCorsa[0];
-
 // // Stampare a schermo la bici con peso minore.
-for (var i = 0; i < biciDaCorsa.length; i++) {
-    if (biciDaCorsa[i].peso < biciLeggera.peso) {
-        biciLeggera = biciDaCorsa[i];
-    }
-}
-
-console.log(biciLeggera);
+var biciPiuLeggera = peso(biciDaCorsa, "peso");
+console.log(biciPiuLeggera);
