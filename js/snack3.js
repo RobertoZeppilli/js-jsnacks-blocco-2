c24c429e665e19ece8904647877d45283ebb54a8
RobertoZeppilli <robzep171@gmail.com>
2021-05-11 17:15:53 +0200

    Snack3 (estremi compresi)

--- js/snack3.js
+++ js/snack3.js
@@ -4,13 +4,13 @@
 function intervalArray(arr, a, b) {
 
     for (var i = 0; i < arr.length; i++) {
-        var newArray = arr.slice(a + 1, b);
+        var newArray = arr.slice(a, b + 1);
     };
     return newArray;
 };
 
 // 2. La funzione restituirà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
-var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+var array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 console.log("Array originale", array);
-var nuovoArray = intervalArray(array, 1, 5);
+var nuovoArray = intervalArray(array, 2, 7);
 console.log("Array tagliato", nuovoArray);
