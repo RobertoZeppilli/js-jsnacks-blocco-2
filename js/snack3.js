3d3b0f47402e7aed5da4ca338b63e98e98b93771
RobertoZeppilli <robzep171@gmail.com>
2021-05-11 15:39:40 +0200

    Snack3 (a + 1, b)

--- js/snack3.js
+++ js/snack3.js
@@ -4,12 +4,12 @@
 function intervalArray(arr, a, b) {
 
     for (var i = 0; i < arr.length; i++) {
-        var newArray = arr.slice(a, b - 1);
+        var newArray = arr.slice(a + 1, b);
     };
     return newArray;
 };
 
-// 2. La funzione restituirà un nuovo array con i valori che hanno la posizione compresa tra i due numeri (inclusi). In caso volessimo escludere i numeri passati al richiamo della funzione, basterebbe scrivere nella funzione  var newArray = arr.slice(a, b - 1);
+// 2. La funzione restituirà un nuovo array con i valori che hanno la posizione compresa tra i due numeri (inclusi).
 var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 console.log("Array originale", array);
 var nuovoArray = intervalArray(array, 5, 10);
