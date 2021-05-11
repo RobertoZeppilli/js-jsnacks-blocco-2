1b16cf5091790f8eba5920d564811785e211de7b
RobertoZeppilli <robzep171@gmail.com>
2021-05-11 16:27:34 +0200

    Snack1 (fixed)

--- js/snack1.js
+++ js/snack1.js
@@ -1,15 +1,5 @@
 // Snack 1
-
-// 1. funzione per ottenere un dato da un array di oggetti, passando come argomenti l'array e due chiavi
-function pesoMinore(arr, key1, key2) {
-    for (var i = 0; i < arr.length; i++) {
-
-        var pesoMinore = "Nome: " + arr[i][key1] + " / " + "Peso: " + Math.min(arr[i][key2]);
-    }
-    return pesoMinore;
-}
-
-// 2. creo l'array di oggetti
+// Creare un array di oggetti:
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 var biciDaCorsa = [
     {
@@ -18,11 +8,11 @@ var biciDaCorsa = [
     },
     {
         nome: "Bianchi",
-        peso: 25
+        peso: 12
     },
     {
         nome: "Trek",
-        peso: 60
+        peso: 2
     },
     {
         nome: "Paco",
@@ -31,18 +21,14 @@ var biciDaCorsa = [
 ]
 console.log(biciDaCorsa);
 
-// --------------------SOLUZIONE SENZA LA FUNZIONE------------------
-// 3. ciclo l'array per ottenere l'oggetto che abbia la chiave "peso" minore tra tutte grazie a Math.min()
-// for (var i = 0; i < biciDaCorsa.length; i++) {
+// inizializzare una variabile biciLeggera per poi comparare nel ciclo for quale peso sia il minore tra tutti
+var biciLeggera = biciDaCorsa[0];
 
-//     var pesoMinore = biciDaCorsa[i].nome + " " + Math.min(biciDaCorsa[i].peso);
-// }
-// console.log(biciLeggera);
-// console.log("La bici che pesa di meno è questa: " + pesoMinore);
-// --------------------SOLUZIONE SENZA LA FUNZIONE------------------
+// // Stampare a schermo la bici con peso minore.
+for (var i = 0; i < biciDaCorsa.length; i++) {
+    if (biciDaCorsa[i].peso < biciLeggera.peso) {
+        biciLeggera = biciDaCorsa[i];
+    }
+}
 
-// --------------------SOLUZIONE CON LA FUNZIONE------------------
-// Stampare a schermo la bici con peso minore.
-var biciLeggera = pesoMinore(biciDaCorsa, "nome", "peso");
 console.log(biciLeggera);
-// --------------------SOLUZIONE CON LA FUNZIONE------------------
